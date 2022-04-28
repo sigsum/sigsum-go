c78997411ffe78ca3fb3aaf646ff1081a5dcd98b
Linus Nordberg <linus@nordberg.se>
2022-04-28 16:48:19 +0200

    rename endpoint get-tree-head-to-sign
    Reference: https://pad.sigsum.org/p/sigsum-db #11

--- pkg/types/endpoint.go
+++ pkg/types/endpoint.go
@@ -8,7 +8,7 @@ const (
 	EndpointAddLeaf             = Endpoint("add-leaf")
 	EndpointAddCosignature      = Endpoint("add-cosignature")
 	EndpointGetTreeHeadLatest   = Endpoint("get-tree-head-latest")
-	EndpointGetTreeHeadToSign   = Endpoint("get-tree-head-to-sign")
+	EndpointGetTreeHeadToSign   = Endpoint("get-tree-head-to-cosign")
 	EndpointGetTreeHeadCosigned = Endpoint("get-tree-head-cosigned")
 	EndpointGetInclusionProof   = Endpoint("get-inclusion-proof/")
 	EndpointGetConsistencyProof = Endpoint("get-consistency-proof/")
