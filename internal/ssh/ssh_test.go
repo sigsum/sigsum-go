293bbf430d1193d21d2666d4629cfe9dbbd846da
Niels Möller <nisse@glasklarteknik.se>
2022-09-30 11:04:12 +0200

    Add test for ssh.SignedData

--- internal/ssh/ssh_test.go
+++ internal/ssh/ssh_test.go
@@ -2,6 +2,7 @@ package ssh
 
 import (
 	"bytes"
+	"encoding/hex"
 	"testing"
 )
 
@@ -25,5 +26,17 @@ func TestSshString(t *testing.T) {
 		if got, want := String(tbl.in), tbl.want; !bytes.Equal(got, want) {
 			t.Errorf("%q: got %x but wanted %x", tbl.desc, got, want)
 		}
+	}
+}
+
+func TestSignedData(t *testing.T) {
+	msg := []byte("foo\n")
+	namespace := "test"
+	got := SignedData(namespace, msg)
+	want, _ := hex.DecodeString("5353485349470000000474657374000000000000000673686132353600000020" +
+		// echo foo | sha256sum
+		"b5bb9d8014a0f9b1d61e21e796d78dccdf1352f23cd32812f4850b878ae4944c")
+	if !bytes.Equal(got, want) {
+		t.Errorf("got %x but wanted %x", got, want)
 	}
 }
