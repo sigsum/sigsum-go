def669307a36c90f17b873e8d6ed382883bf25b0
Niels Möller <nisse@glasklarteknik.se>
2022-11-07 11:07:20 +0100

    Add test of ascii.Parser

--- pkg/ascii/parser_test.go
+++ pkg/ascii/parser_test.go
@@ -2,6 +2,10 @@ package ascii
 
 import (
 	"testing"
+
+	"bytes"
+	"fmt"
+	"sigsum.org/sigsum-go/pkg/crypto"
 )
 
 func TestValidIntFromDecimal(t *testing.T) {
@@ -39,5 +43,38 @@ func TestInvalidIntFromDecimal(t *testing.T) {
 			t.Errorf("no error on invalid input %q, got %d",
 				in, x)
 		}
+	}
+}
+
+func TestParser(t *testing.T) {
+	hash := crypto.HashBytes([]byte("x"))
+	input := fmt.Sprintf("hash=%x\nint=12345\nvalues=a b c\n", hash)
+
+	p := NewParser(bytes.NewBuffer([]byte(input)))
+	if got, err := p.GetHash("hash"); err != nil || got != hash {
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Errorf("bad hash, got %x, wanted %x", got, hash)
+	}
+
+	if got, err := p.GetInt("int"); err != nil || got != 12345 {
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Errorf("bad int, got %d, wanted %d", got, 12345)
+	}
+	v, err := p.GetValues("values", 3)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(v) != 3 {
+		t.Errorf("unexpected number of values (wanted 3): %#v", v)
+	}
+	if v[0] != "a" || v[1] != "b" || v[2] != "c" {
+		t.Errorf("unexpected values (wanted a, b,c): %#v", v)
+	}
+	if err := p.GetEOF(); err != nil {
+		t.Errorf("GetEOF failure: %v", err)
 	}
 }
