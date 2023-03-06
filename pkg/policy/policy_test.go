e42ac2f7b09dec7697271f3bf6cc51463e0ae81c
Niels Möller <nisse@glasklarteknik.se>
2023-03-06 15:48:57 +0100

    Add test for witness k-of-n logic

--- pkg/policy/policy_test.go
+++ pkg/policy/policy_test.go
@@ -44,3 +44,73 @@ func TestLogPolicy(t *testing.T) {
 		t.Errorf("verifying treehead for log 0 with log hash 1 succeeeded")
 	}
 }
+
+func TestWitnessPolicy(t *testing.T) {
+	th := types.TreeHead{Size: 3}
+	logPub, logSigner, err := crypto.NewKeyPair()
+	if err != nil {
+		t.Fatal(err)
+	}
+	logHash := crypto.HashBytes(logPub[:])
+
+	sth, err := th.Sign(logSigner)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var witnessKeys []crypto.PublicKey
+	var witnessHashes []crypto.Hash
+	var cosignatures []types.Cosignature
+
+	for i := 0; i < 5; i++ {
+		pub, s, err := crypto.NewKeyPair()
+		if err != nil {
+			t.Fatal(err)
+		}
+		cosignature, err := th.Cosign(s, &logHash, 0)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		cosignatures = append(cosignatures, cosignature)
+		witnessKeys = append(witnessKeys, pub)
+		witnessHashes = append(witnessHashes, crypto.HashBytes(pub[:]))
+	}
+	// Four known witnesses, at least 3 cosignatures required.
+	p, err := NewKofNPolicy([]crypto.PublicKey{logPub},
+		witnessKeys[:4], 3)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, s := range []struct {
+		desc        string
+		w           []int // Indices of witnesses to include
+		invalidate  int   // Signature to invalidate (-1 if none)
+		expectValid bool
+	}{
+		{"no cosignature", nil, -1, false},
+		{"only one cosignature", []int{0}, -1, false},
+		{"only two cosignatures", []int{0, 1, 4}, -1, false},
+		{"three cosignature", []int{0, 1, 2}, -1, true},
+		{"other three cosignature", []int{1, 2, 3}, -1, true},
+		{"all cosignatures", []int{0, 1, 2, 3, 4}, 4, true},
+		{"all cosignatures, one invalid", []int{0, 1, 2, 3, 4}, 2, true},
+		{"three cosignatures, but one invalid", []int{0, 2, 3, 4}, 2, false},
+	} {
+		var present []types.Cosignature
+		for _, i := range s.w {
+			present = append(present, cosignatures[i])
+			if i == s.invalidate {
+				present[len(present)-1].Signature[3] ^= 1
+			}
+		}
+		err := p.VerifyCosignedTreeHead(&logHash,
+			&types.CosignedTreeHead{SignedTreeHead: sth, Cosignatures: present})
+		if s.expectValid && err != nil {
+			t.Errorf("%s: Failed on valid cth: %v", s.desc, err)
+		}
+		if !s.expectValid && err == nil {
+			t.Errorf("%s: Expected error, but got none", s.desc)
+		}
+	}
+}
