1fb1133a3563fe679622da35fcde56b2a173e0bd
Niels Möller <nisse@glasklarteknik.se>
2023-03-20 14:02:01 +0100

    Delete unused features of sigsum-debug

--- cmd/sigsum-debug/head/head.go
+++ cmd/sigsum-debug/head/head.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"time"
 
-	"sigsum.org/sigsum-go/cmd/sigsum-debug/head/consistency"
 	"sigsum.org/sigsum-go/cmd/sigsum-debug/head/sign"
 	"sigsum.org/sigsum-go/internal/options"
 )
@@ -21,15 +20,11 @@ Usage:
 
   sigsum-debug head sign -k PRIVATE_KEY -h KEY_HASH [-t TIMESTAMP]
     Reads an ascii signed tree head from stdin and outputs a new signature
-
-  sigsum-debug head consistency -n OLD_SIZE -N NEW_SIZE -r OLD_ROOT -R NEW_ROOT
-    Reads an ascii consistency proof from stdin and verifies it
-
 `
 
 var (
-	optPrivateKey, optPublicKey, optKeyHash, optOldRoot, optNewRoot string
-	optOldSize, optNewSize, optTimestamp                            uint64
+	optPrivateKey, optKeyHash string
+	optTimestamp              uint64
 )
 
 func Main(args []string) error {
@@ -43,8 +38,6 @@ func Main(args []string) error {
 			opt.Usage()
 		case "sign":
 			err = sign.Main(opt.Args(), optPrivateKey, optKeyHash, optTimestamp)
-		case "consistency":
-			err = consistency.Main(opt.Args(), optOldSize, optNewSize, optOldRoot, optNewRoot)
 		default:
 			err = fmt.Errorf("invalid command %q, try \"help\"", opt.Name())
 		}
@@ -67,13 +60,6 @@ func setOptions(fs *flag.FlagSet) {
 		options.AddString(fs, &optPrivateKey, "k", "private-key", options.DefaultString)
 		options.AddString(fs, &optKeyHash, "h", "key-hash", options.DefaultString)
 		options.AddUint64(fs, &optTimestamp, "t", "timestamp", uint64(time.Now().Unix()))
-	case "verify":
-		options.AddString(fs, &optPublicKey, "k", "public-key", options.DefaultString)
-	case "consistency":
-		options.AddUint64(fs, &optOldSize, "n", "old-size", options.DefaultUint64)
-		options.AddUint64(fs, &optNewSize, "N", "new-size", options.DefaultUint64)
-		options.AddString(fs, &optOldRoot, "r", "old-root", options.DefaultString)
-		options.AddString(fs, &optNewRoot, "R", "new-root", options.DefaultString)
 	}
 }
 
@@ -86,13 +72,6 @@ func checkOptions(cmd string) error {
 	case "sign":
 		err = options.CheckString("private key", optPrivateKey, err)
 		err = options.CheckString("key hash", optKeyHash, err)
-	case "verify":
-		err = options.CheckString("public key", optPublicKey, err)
-	case "consistency":
-		err = options.CheckUint64("old size", optOldSize, err)
-		err = options.CheckUint64("new size", optNewSize, err)
-		err = options.CheckString("old root", optOldRoot, err)
-		err = options.CheckString("new root", optNewRoot, err)
 	}
 
 	return err
