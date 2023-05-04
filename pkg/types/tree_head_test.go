2b811a66d321bcb3158d81b397ce10c421c82c67
Niels Möller <nisse@glasklarteknik.se>
2023-05-04 09:42:20 +0200

    Use checkpoint-based format for cosigned data

--- pkg/types/tree_head_test.go
+++ pkg/types/tree_head_test.go
@@ -18,8 +18,10 @@ const (
 
 func TestTreeHeadToCheckpoint(t *testing.T) {
 	desc := "valid"
-	if got, want := validTreeHead(t).toCheckpoint(&crypto.PublicKey{}),
-		validTreeHeadCheckpoint(t); !bytes.Equal(got, want) {
+	pub := crypto.PublicKey{}
+	keyHash := crypto.HashBytes(pub[:])
+	if got, want := validTreeHead(t).toCheckpoint(&keyHash),
+		validTreeHeadCheckpoint(t); got != want {
 		t.Errorf("got tree head checkpoint\n\t%q\nbut wanted\n\t%q\nin test %q\n", got, want, desc)
 	}
 }
@@ -298,12 +300,12 @@ func validTreeHead(t *testing.T) *TreeHead {
 	}
 }
 
-func validTreeHeadCheckpoint(t *testing.T) []byte {
-	return []byte(`
+func validTreeHeadCheckpoint(t *testing.T) string {
+	return `
 sigsum.org/v1/tree/66687aadf862bd776c8fc18b8e9f8e20089714856ee233b3902a591d0d5f2925
 257
 AAECAwQFBgcICQoLDA0ODxAREhMUFRYXGBkaGxwdHh8=
-`)[1:]
+`[1:]
 }
 
 func validSignedTreeHead(t *testing.T) *SignedTreeHead {
