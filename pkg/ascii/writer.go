b914bcadbb88abff3053997dd14f12e41646cef3
Filippo Valsorda <hi@filippo.io>
2023-05-11 16:50:01 +0200

    pkg/ascii: improve edge cases
    Return all writing-path errors, and be more strict on parsing.
    
    Fix SigsumProof.ToASCII, that was relying on the parser accepting an
    unterminated final line.

--- pkg/ascii/writer.go
+++ pkg/ascii/writer.go
@@ -10,21 +10,24 @@ import (
 func writeItem(w io.Writer, item interface{}) error {
 	switch item := item.(type) {
 	case []byte:
-		fmt.Fprintf(w, "%x", item)
-		return nil
+		_, err := fmt.Fprintf(w, "%x", item)
+		return err
 	case uint64:
 		if item >= (1 << 63) {
 			return fmt.Errorf("out of range number: %d", item)
 		}
-		fmt.Fprintf(w, "%d", item)
-		return nil
+		_, err := fmt.Fprintf(w, "%d", item)
+		return err
 	default:
 		return fmt.Errorf("unsupported type: %t", item)
 	}
 }
 
 func WriteLine(w io.Writer, key string, first interface{}, rest ...interface{}) error {
-	fmt.Fprintf(w, "%s=", key)
+	_, err := fmt.Fprintf(w, "%s=", key)
+	if err != nil {
+		return err
+	}
 	if err := writeItem(w, first); err != nil {
 		return err
 	}
@@ -34,11 +37,12 @@ func WriteLine(w io.Writer, key string, first interface{}, rest ...interface{}) 
 			return err
 		}
 	}
-	_, err := fmt.Fprintf(w, "\n")
+	_, err = fmt.Fprintf(w, "\n")
 	return err
 }
 
-// Redundant, except for beter type checking.
+// Helpers with better type safety.
+
 func WriteInt(w io.Writer, name string, i uint64) error {
 	return WriteLine(w, name, i)
 }
