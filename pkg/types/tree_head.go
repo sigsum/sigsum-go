1d8dc2ae417909528139a1263ad551d1e6d6186d
Niels Möller <nisse@glasklarteknik.se>
2023-05-25 13:54:50 +0200

    Add cosignature version (currently always "v1")

--- pkg/types/tree_head.go
+++ pkg/types/tree_head.go
@@ -3,6 +3,7 @@ package types
 import (
 	"encoding/base64"
 	"encoding/binary"
+	"errors"
 	"fmt"
 	"io"
 
@@ -15,6 +16,11 @@ import (
 const (
 	CosignatureNamespace = "cosignature/v1"
 	CheckpointNamePrefix = "sigsum.org/v1/tree/"
+	CosignatureVersion   = "v1"
+)
+
+var (
+	ErrUnknownCosignatureVersion = errors.New("unexpected cosignature version")
 )
 
 type TreeHead struct {
@@ -28,6 +34,9 @@ type SignedTreeHead struct {
 }
 
 type Cosignature struct {
+	// Note that on the wire, there's also a version field. That
+	// is not represented here, since the current implementation
+	// supports only the "v1" version.
 	KeyHash   crypto.Hash
 	Timestamp uint64
 	Signature crypto.Signature
@@ -163,23 +172,26 @@ func (cs *Cosignature) Verify(key *crypto.PublicKey, logKeyHash *crypto.Hash, th
 }
 
 func (cs *Cosignature) ToASCII(w io.Writer) error {
-	return ascii.WriteLine(w, "cosignature", cs.KeyHash[:], cs.Timestamp, cs.Signature[:])
+	return ascii.WriteLine(w, "cosignature", CosignatureVersion, cs.KeyHash[:], cs.Timestamp, cs.Signature[:])
 }
 
 func (cs *Cosignature) Parse(p *ascii.Parser) error {
-	v, err := p.GetValues("cosignature", 3)
+	v, err := p.GetValues("cosignature", 4)
 	if err != nil {
 		return err
 	}
-	cs.KeyHash, err = crypto.HashFromHex(v[0])
+	if v[0] != CosignatureVersion {
+		return ErrUnknownCosignatureVersion
+	}
+	cs.KeyHash, err = crypto.HashFromHex(v[1])
 	if err != nil {
 		return err
 	}
-	cs.Timestamp, err = ascii.IntFromDecimal(v[1])
+	cs.Timestamp, err = ascii.IntFromDecimal(v[2])
 	if err != nil {
 		return err
 	}
-	cs.Signature, err = crypto.SignatureFromHex(v[2])
+	cs.Signature, err = crypto.SignatureFromHex(v[3])
 	return err
 }
 
@@ -211,6 +223,10 @@ func ParseCosignatures(p *ascii.Parser) ([]Cosignature, error) {
 		err := cs.Parse(p)
 		if err == io.EOF {
 			return cosignatures, nil
+		}
+		if err == ErrUnknownCosignatureVersion {
+			// Silently ignore
+			continue
 		}
 		if err != nil {
 			return nil, err
