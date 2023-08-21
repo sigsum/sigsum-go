c21b9186d973438cd734c433b34219ff81ed28fa
Niels Möller <nisse@glasklarteknik.se>
2023-08-21 15:50:15 +0200

    Add a first unit test for monitoringLogClient

---
+++ pkg/monitor/client_test.go
@@ -0,0 +1,128 @@
+package monitor
+
+import (
+	"context"
+	"encoding/binary"
+	"fmt"
+	"math/rand"
+	"testing"
+
+	"sigsum.org/sigsum-go/pkg/api"
+	"sigsum.org/sigsum-go/pkg/crypto"
+	"sigsum.org/sigsum-go/pkg/merkle"
+	"sigsum.org/sigsum-go/pkg/requests"
+	token "sigsum.org/sigsum-go/pkg/submit-token"
+	"sigsum.org/sigsum-go/pkg/types"
+)
+
+// Implements client.Log.
+// TODO: Move to some public package, and add an RWMutex for
+// syncronization.
+type testLog struct {
+	leaves []types.Leaf
+	tree   merkle.Tree
+	signer crypto.Signer
+}
+
+func (l *testLog) GetTreeHead(_ context.Context) (types.CosignedTreeHead, error) {
+	th := types.TreeHead{
+		Size:     uint64(l.tree.Size()),
+		RootHash: l.tree.GetRootHash(),
+	}
+	sth, err := th.Sign(l.signer)
+	return types.CosignedTreeHead{SignedTreeHead: sth}, err
+}
+
+func (l *testLog) GetInclusionProof(_ context.Context, req requests.InclusionProof) (types.InclusionProof, error) {
+	index, err := l.tree.GetLeafIndex(&req.LeafHash)
+	if err != nil || index >= req.Size {
+		return types.InclusionProof{}, api.ErrNotFound
+	}
+	path, err := l.tree.ProveInclusion(index, req.Size)
+	return types.InclusionProof{
+		LeafIndex: index,
+		Path:      path,
+	}, err
+}
+
+func (l *testLog) GetConsistencyProof(_ context.Context, req requests.ConsistencyProof) (types.ConsistencyProof, error) {
+	path, err := l.tree.ProveConsistency(req.OldSize, req.NewSize)
+	return types.ConsistencyProof{Path: path}, err
+}
+
+func (l *testLog) GetLeaves(_ context.Context, req requests.Leaves) ([]types.Leaf, error) {
+	size := l.tree.Size()
+	if req.StartIndex >= size || req.EndIndex > size || req.StartIndex >= req.EndIndex {
+		return nil, fmt.Errorf("out of range request: start %d, end %d, size %d\n",
+			req.StartIndex, req.EndIndex, size)
+	}
+	return l.leaves[req.StartIndex:req.EndIndex], nil
+}
+
+func (l *testLog) AddLeaf(_ context.Context, req requests.Leaf, _ *token.SubmitHeader) (bool, error) {
+	leaf, err := req.Verify()
+	if err != nil {
+		return false, api.ErrForbidden
+	}
+
+	h := leaf.ToHash()
+	if l.tree.AddLeafHash(&h) {
+		l.leaves = append(l.leaves, leaf)
+	}
+	return true, nil
+}
+
+func makeLeafRequest(t *testing.T, signer crypto.Signer, msg *crypto.Hash) requests.Leaf {
+	signature, err := types.SignLeafMessage(signer, msg[:])
+	if err != nil {
+		t.Fatalf("Leaf signing failed: %v\n", err)
+	}
+	return requests.Leaf{
+		Message:   *msg,
+		Signature: signature,
+		PublicKey: signer.Public(),
+	}
+}
+
+// Test for successful case.
+func TestGetTreeHead(t *testing.T) {
+	logSigner := crypto.NewEd25519Signer(&crypto.PrivateKey{2})
+	leafSigner := crypto.NewEd25519Signer(&crypto.PrivateKey{3})
+	log := testLog{signer: logSigner, tree: merkle.NewTree()}
+
+	monitorClient := monitoringLogClient{
+		logKey: logSigner.Public(),
+		client: &log,
+	}
+	r := rand.New(rand.NewSource(10))
+
+	prevTree := types.NewEmptyTreeHead()
+
+	for i := 0; i < 100; i++ {
+		// Ensures that batch is of zero size, so that first
+		// GetTreeHead returns an empty tree.
+		c := r.Intn(i + 1)
+		newSize := log.tree.Size() + uint64(c)
+		for j := 0; j < c; j++ {
+			var msg crypto.Hash
+			binary.BigEndian.PutUint64(msg[:], uint64(i))
+			binary.BigEndian.PutUint64(msg[8:], uint64(j))
+			_, err := log.AddLeaf(context.Background(), makeLeafRequest(t, leafSigner, &msg), nil)
+			if err != nil {
+				t.Fatalf("AddLeaf failed: %v", err)
+			}
+		}
+		if got, want := log.tree.Size(), newSize; got != want {
+			t.Fatalf("Unexpected merkle tree size: got %d, want %d", got, want)
+		}
+
+		sth, err := monitorClient.getTreeHead(context.Background(), &prevTree)
+		if err != nil {
+			t.Fatalf("GetTreeHead failed: %v", err)
+		}
+		if got, want := sth.Size, newSize; got != want {
+			t.Fatalf("Unexpected log size: got %d, want %d", got, want)
+		}
+		prevTree = sth.TreeHead
+	}
+}
