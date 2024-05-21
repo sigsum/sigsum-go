cbe2a5dfb1826571acb49c7b4a20655dcb7e0efa
Niels Möller <nisse@glasklarteknik.se>
2024-05-21 16:06:49 +0200

    Refactor, delete non-batch method

--- pkg/submit/submit.go
+++ pkg/submit/submit.go
@@ -1,20 +1,15 @@
 // package submit acts as a sigsum submit client
-// It submits a leaf to a log, and collects a sigsum proof.
+// It submits one or more leaf to the logs specified by policy, and
+// collects corresponding sigsum proofs.
 package submit
 
 import (
-	"context"
 	"time"
 
-//	"sigsum.org/sigsum-go/pkg/api"
-//	"sigsum.org/sigsum-go/pkg/client"
+	"net/http"
+
 	"sigsum.org/sigsum-go/pkg/crypto"
-	"sigsum.org/sigsum-go/pkg/log"
 	"sigsum.org/sigsum-go/pkg/policy"
-	"sigsum.org/sigsum-go/pkg/proof"
-	"sigsum.org/sigsum-go/pkg/requests"
-//	token "sigsum.org/sigsum-go/pkg/submit-token"
-	"sigsum.org/sigsum-go/pkg/types"
 )
 
 const (
@@ -36,9 +31,10 @@ type Config struct {
 
 	// Delay when repeating add-leaf requests to the log, as well
 	// as for polling for a cosigned tree head and inclusion
-	// proof.
+	// proof. Zero implies a default timeout is used.
 	PollDelay time.Duration
 
+	// HTTP user agent, empty implies a default user agent string.
 	UserAgent string
 
 	// The policy specifies the logs and witnesses to use.
@@ -49,73 +45,17 @@ type Config struct {
 	HTTPClient *http.Client
 }
 
-func (c *Config) getPollDelay() time.Duration {
+func (c *Config) withDefaults() Config {
+	res := *c
+
 	if c.PollDelay <= 0 {
-		return defaultPollDelay
+		res.PollDelay = defaultPollDelay
 	}
-	return c.PollDelay
-}
-
-func (c *Config) getTimeout() time.Duration {
 	if c.PerLogTimeout <= 0 {
-		return defaultTimeout
+		res.PerLogTimeout = defaultTimeout
 	}
-	return c.PerLogTimeout
-}
-
-func (c *Config) getUserAgent() string {
-	if len(c.UserAgent) == 0 {
-		return defaultUserAgent
+	if c.UserAgent == "" {
+		res.UserAgent = defaultUserAgent
 	}
-	return c.UserAgent
-}
-
-// Sleep for the given delay, but fail early if the context is
-// cancelled.
-func sleepWithContext(ctx context.Context, d time.Duration) error {
-	timer := time.NewTimer(d)
-	defer timer.Stop()
-
-	select {
-	case <-timer.C:
-		return nil
-	case <-ctx.Done():
-		return ctx.Err()
-	}
-}
-
-func (c *Config) sleep(ctx context.Context) error {
-	return sleepWithContext(ctx, c.getPollDelay())
-}
-
-
-func SubmitMessage(ctx context.Context, config *Config, signer crypto.Signer, message *crypto.Hash) (proof.SigsumProof, error) {
-	signature, err := types.SignLeafMessage(signer, message[:])
-	if err != nil {
-		return proof.SigsumProof{}, err
-	}
-	return SubmitLeafRequest(ctx, config, &requests.Leaf{
-		Message:   *message,
-		Signature: signature,
-		PublicKey: signer.Public(),
-	})
-}
-
-func SubmitLeafRequest(ctx context.Context, config *Config, req *requests.Leaf) (proof.SigsumProof, error) {
-	log.Debug("Creating batch");
-	b, err := NewBatch(ctx, config)	
-	if err != nil {
-		return proof.SigsumProof{}, err
-	}
-	defer b.Close()
-	var pr proof.SigsumProof
-	log.Debug("Submitting request");
-	if err := b.SubmitLeafRequest(req, func(res proof.SigsumProof) { pr = res }); err != nil {
-		return proof.SigsumProof{}, err
-	}
-	log.Debug("Waiting")
-	if err := b.Close(); err != nil {
-		return proof.SigsumProof{}, err
-	}
-	return pr, nil
+	return res
 }
