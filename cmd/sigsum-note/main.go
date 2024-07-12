c57c98c3d0694dabe28b6498cc6c48d58c69228c
Grégoire Détrez <gregoire@mullvad.net>
2024-07-12 12:14:42 +0200

    PoC Spicy Signed Notes

---
+++ cmd/sigsum-note/main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"context"
+	"encoding/binary"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"golang.org/x/mod/sumdb/note"
+	"sigsum.org/sigsum-go/pkg/crypto"
+	"sigsum.org/sigsum-go/pkg/key"
+	"sigsum.org/sigsum-go/pkg/policy"
+	"sigsum.org/sigsum-go/pkg/proof"
+	"sigsum.org/sigsum-go/pkg/submit"
+)
+
+var keyFile = flag.String("k", "", "Private key file")
+var keyName = flag.String("n", "", "Key name")
+var policyFile = flag.String("p", "", "Policy file")
+
+func main() {
+	flag.Parse()
+	text, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		log.Fatal(err)
+	}
+	cryptoSigner, err := key.ReadPrivateKeyFile(*keyFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	policy, err := policy.ReadPolicyFile(*policyFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	noteSigner := &NoteSigner{
+		name:         *keyName,
+		ctx:          context.TODO(),
+		cryptoSigner: cryptoSigner,
+		submitConfig: &submit.Config{Policy: policy},
+	}
+	n, err := note.Sign(&note.Note{Text: string(text)}, noteSigner)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Print(string(n))
+}
+
+type NoteSigner struct {
+	name         string
+	ctx          context.Context
+	cryptoSigner crypto.Signer
+	submitConfig *submit.Config
+}
+
+// Name is part of the note.Signer interface
+func (s *NoteSigner) Name() string { return s.name }
+
+// KeyHash is part of the note.Signer interface
+func (s *NoteSigner) KeyHash() uint32 {
+	buf := []byte(s.Name())
+	buf = append(buf, 0x0A, 0xFF)
+	buf = append(buf, []byte("SIGSUMv1")...)
+	pk := s.cryptoSigner.Public()
+	buf = append(buf, pk[:]...)
+	return binary.BigEndian.Uint32(buf[:4])
+}
+
+// Sign is part of the note.Signer interface
+func (s *NoteSigner) Sign(msg []byte) ([]byte, error) {
+	sigsumMsg := crypto.HashBytes(msg)
+	spicysig, err := submit.SubmitMessage(s.ctx, s.submitConfig, s.cryptoSigner, &sigsumMsg)
+	if err != nil {
+		return nil, err
+	}
+	return SerializeSpicySignature(spicysig)
+}
+
+func SerializeSpicySignature(sig proof.SigsumProof) ([]byte, error) {
+	buf := []byte{}
+	// Signature
+	buf = append(buf, sig.Leaf.Signature[:]...)
+
+	// Inclusion Proof
+	buf = binary.BigEndian.AppendUint64(buf, sig.Inclusion.LeafIndex)
+	buf = binary.BigEndian.AppendUint16(buf, uint16(len(sig.Inclusion.Path)))
+	for _, h := range sig.Inclusion.Path {
+		buf = append(buf, h[:]...)
+	}
+
+	// Signed tree head
+	buf = append(buf, sig.LogKeyHash[:]...)
+	buf = binary.BigEndian.AppendUint64(buf, sig.TreeHead.Size)
+	buf = append(buf, sig.TreeHead.RootHash[:]...)
+	buf = append(buf, sig.TreeHead.Signature[:]...)
+	buf = binary.BigEndian.AppendUint16(buf, uint16(len(sig.TreeHead.Cosignatures)))
+	for _, cs := range sig.TreeHead.Cosignatures {
+		buf = append(buf, cs.KeyHash[:]...)
+		buf = binary.BigEndian.AppendUint64(buf, cs.Timestamp)
+		buf = append(buf, cs.Signature[:]...)
+	}
+	return buf, nil
+}
--- go.mod
+++ go.mod
@@ -8,12 +8,13 @@ require (
 	github.com/dchest/safefile v0.0.0-20151022103144-855e8d98f185
 	github.com/golang/mock v1.6.0
 	github.com/pborman/getopt/v2 v2.1.0
+	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4
 	golang.org/x/net v0.5.0
 	golang.org/x/text v0.6.0
 )
 
 require (
-	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
 	golang.org/x/sys v0.4.0 // indirect
 	golang.org/x/tools v0.1.12 // indirect
 )
