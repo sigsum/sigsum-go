14c07e9d8d52132bcfb312d21a6022b50059ee3b
Niels Möller <nisse@glasklarteknik.se>
2024-10-22 13:26:08 +0200

    Add tests for FromURLArgs methods

--- pkg/requests/requests_test.go
+++ pkg/requests/requests_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"strings"
 	"testing"
 
 	"sigsum.org/sigsum-go/pkg/crypto"
@@ -76,6 +77,153 @@ func TestLeafFromASCII(t *testing.T) {
 		}
 		if got, want := leaf, *table.want; got != want {
 			t.Errorf("got leaf request\n\t%v\nbut wanted\n\t%v\nin test %q\n", got, want, table.desc)
+		}
+	}
+}
+
+func TestLeavesFromURLArgs(t *testing.T) {
+	for _, table := range []struct {
+		desc       string
+		start, end string
+		want       *Leaves
+		wantErr    string
+	}{
+		{
+			desc:    "bad start",
+			start:   "x",
+			end:     "12",
+			wantErr: "parsing \"x\"",
+		},
+		{
+			desc:    "bad end",
+			start:   "1",
+			end:     "-2",
+			wantErr: "parsing \"-2\"",
+		},
+		{
+			desc:  "valid range",
+			start: "1",
+			end:   "20",
+			want:  &Leaves{StartIndex: 1, EndIndex: 20},
+		},
+		{
+			desc:  "valid syntax, improper range",
+			start: "20",
+			end:   "1",
+			want:  &Leaves{StartIndex: 20, EndIndex: 1},
+		},
+	} {
+		var leaves Leaves
+		err := leaves.FromURLArgs(table.start, table.end)
+		if table.want != nil {
+			if err != nil {
+				t.Errorf("test %s: %v", table.desc, err)
+			} else if leaves != *table.want {
+				t.Errorf("test %s: got %v, want %v", table.desc, leaves, *table.want)
+			}
+		} else if err == nil {
+			t.Errorf("test %s: expected err, got result %v", table.desc, leaves)
+		} else if !strings.Contains(err.Error(), table.wantErr) {
+			t.Errorf("test %s: expected err %q, got %v", table.desc, table.wantErr, err)
+		}
+	}
+}
+
+func TestInclusionProofFromURLArgs(t *testing.T) {
+	for _, table := range []struct {
+		desc       string
+		size, hash string
+		want       *InclusionProof
+		wantErr    string
+	}{
+		{
+			desc:    "bad size",
+			size:    "x",
+			hash:    "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+			wantErr: "parsing \"x\"",
+		},
+		{
+			desc:    "bad hash",
+			size:    "10",
+			hash:    "aaaaa",
+			wantErr: "odd length",
+		},
+		{
+			desc: "valid",
+			size: "10",
+			hash: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+			want: &InclusionProof{
+				Size: 10,
+				LeafHash: crypto.Hash{
+					170, 170, 170, 170, 170, 170, 170, 170,
+					170, 170, 170, 170, 170, 170, 170, 170,
+					170, 170, 170, 170, 170, 170, 170, 170,
+					170, 170, 170, 170, 170, 170, 170, 170,
+				},
+			},
+		},
+	} {
+		var proof InclusionProof
+		err := proof.FromURLArgs(table.size, table.hash)
+		if table.want != nil {
+			if err != nil {
+				t.Errorf("test %s: %v", table.desc, err)
+			} else if proof != *table.want {
+				t.Errorf("test %s: got %v, want %v", table.desc, proof, *table.want)
+			}
+		} else if err == nil {
+			t.Errorf("test %s: expected err, got result %v", table.desc, proof)
+		} else if !strings.Contains(err.Error(), table.wantErr) {
+			t.Errorf("test %s: expected err %q, got %v", table.desc, table.wantErr, err)
+		}
+	}
+
+}
+
+func TestConsistencyProofFromURLArgs(t *testing.T) {
+	for _, table := range []struct {
+		desc     string
+		old, new string
+		want     *ConsistencyProof
+		wantErr  string
+	}{
+		{
+			desc:    "bad old",
+			old:     "x",
+			new:     "12",
+			wantErr: "parsing \"x\"",
+		},
+		{
+			desc:    "bad new",
+			old:     "1",
+			new:     "-2",
+			wantErr: "parsing \"-2\"",
+		},
+		{
+			desc: "valid range",
+			old:  "1",
+			new:  "20",
+			want: &ConsistencyProof{OldSize: 1, NewSize: 20},
+		},
+		{
+			desc: "valid syntax, improper range",
+			old:  "20",
+			new:  "1",
+			want: &ConsistencyProof{OldSize: 20, NewSize: 1},
+		},
+	} {
+		var proof ConsistencyProof
+		err := proof.FromURLArgs(table.old, table.new)
+		if table.want != nil {
+			if err != nil {
+				t.Errorf("test %s: %v", table.desc, err)
+			} else if proof != *table.want {
+				t.Errorf("test %s: got %v, want %v", table.desc, proof, *table.want)
+			}
+		} else if err == nil {
+			t.Errorf("test %s: expected err, got result %v", table.desc, proof)
+		} else if !strings.Contains(err.Error(), table.wantErr) {
+			t.Errorf("test %s: expected err %q, got %v", table.desc, table.wantErr, err)
 		}
 	}
 }
