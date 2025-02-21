dd71582fd1ea23641b0716708d970d83bb0afab4
Rasmus Dahlberg <rgdd@glasklarteknik.se>
2025-02-21 12:39:04 +0100

    sigsum-token: Improve UI and usage messages
    - Make a briefer subcommand summary that looks nice on manpage
    - Light editing of subcommand descriptions
    - Add some of the missing long options
    - Elaborate and be more precise in usage message

--- cmd/sigsum-token/sigsum-token.go
+++ cmd/sigsum-token/sigsum-token.go
@@ -36,25 +36,21 @@ type verifySettings struct {
 }
 
 func main() {
-	const usage = `sigsum-token sub commands:
+	const usage = `
+Utility commands that help configure DNS and create/verify tokens.
+Such setup allows log servers to apply rate-limits per domain name.
 
-sigsum-token help | --help
-  Display this help. All the below sub commands also accept the --help
-  option, to display help for that sub command.
+Note that sigsum-submit can create tokens on-the-fly.  As a user you
+mainly need to setup a DNS TXT record, see the record subcommand.
 
-sigsum-token version | --version | -v
-  Display software version.
+Usage: sigsum-token [--help|help] [--version|version]
+   or: sigsum-token create [options]
+   or: sigsum-token record [options]
+   or: sigsum-token verify [options] < token
 
-sigsum-token create [options]
-  Create a token for submissions to the the given log, essentially
-  a signature on the log's public key.
-
-sigsum-token record [options]
-  Format a public key as a TXT record in zone file format.
-
-sigsum-token verify [options] < token
-  Verifies a submit token. The input on stdin is either a raw hex
-  token or a HTTP header.
+Options:
+      --help     Show usage message and exit
+  -v, --version  Show program version and exit
 `
 	log.SetFlags(0)
 	if len(os.Args) < 2 {
@@ -63,9 +59,9 @@ sigsum-token verify [options] < token
 
 	switch os.Args[1] {
 	default:
-		log.Fatal(usage)
+		log.Fatal(usage[1:])
 	case "help", "--help":
-		fmt.Print(usage)
+		fmt.Print(usage[1:])
 		os.Exit(0)
 	case "version", "--version", "-v":
 		version.DisplayVersion("sigsum-token")
@@ -196,38 +192,38 @@ func parseNoArgs(set *getopt.Set, args []string, usage string) {
 
 func (s *createSettings) parse(args []string) {
 	set := newOptionSet(args, "")
-	set.FlagLong(&s.keyFile, "signing-key", 'k', "Private key for signing", "file").Mandatory()
-	set.Flag(&s.outputFile, 'o', "Output", "file")
-	set.FlagLong(&s.logKeyFile, "log-key", 0, "Log's public key", "file").Mandatory()
-	set.FlagLong(&s.domain, "domain", 0, "Domain")
+	set.FlagLong(&s.keyFile, "signing-key", 'k', "Private key in OpenSSH format to sign the token with; or a corresponding public key where the private part is accessed using the SSH agent protocol", "key-file").Mandatory()
+	set.FlagLong(&s.outputFile, "output", 'o', "Store output in a file", "output-file")
+	set.FlagLong(&s.logKeyFile, "log-key", 'l', "The log's public key in OpenSSH format", "key-file").Mandatory()
+	set.FlagLong(&s.domain, "domain", 'd', "Domain name where the rate-limit public-key is available as a TXT record; without any \"_sigsum_v1.\" prefix", "domain-name")
 	parseNoArgs(set, args, `
-Create a token for submissions to the the given log, essentially a
-signature on the log's public key. If --domain is given, output a
-complete HTTP header.
+Create a token for submissions to the the given log.  This is
+essentially a signature on the log's public key.  Outputs a complete
+HTTP header if a domain name is provided (-d option).
 `)
 }
 
 func (s *recordSettings) parse(args []string) {
 	set := newOptionSet(args, "")
-	set.FlagLong(&s.keyFile, "key", 'k', "Public key", "file").Mandatory()
-	set.Flag(&s.outputFile, 'o', "Output", "file")
+	set.FlagLong(&s.keyFile, "key", 'k', "Rate-limit public-key in OpenSSH format", "key-file").Mandatory()
+	set.FlagLong(&s.outputFile, "output", 'o', "Store output in a file", "output-file")
 	parseNoArgs(set, args, `
-Format the public key as a TXT record in zone file format.
+Format the rate-limit key as a TXT record in zone-file format.
 `)
 }
 
 func (s *verifySettings) parse(args []string) {
-	set := newOptionSet(args, "< token")
-	set.FlagLong(&s.keyFile, "key", 'k', "Public key", "file")
-	set.FlagLong(&s.logKeyFile, "log-key", 0, "Log's public key", "file").Mandatory()
-	set.FlagLong(&s.domain, "domain", 0, "Domain")
+	set := newOptionSet(args, "<token")
+	set.FlagLong(&s.keyFile, "key", 'k', "Rate-limit public-key in OpenSSH format", "key-file")
+	set.FlagLong(&s.logKeyFile, "log-key", 'l', "Log public-key in OpenSSH format", "key-file").Mandatory()
+	set.FlagLong(&s.domain, "domain", 'd', "Domain name where the rate-limit key is available as a TXT record; without any \"_sigsum_v1.\" prefix", "domain-name")
 	set.FlagLong(&s.quiet, "quiet", 'q', "Quiet mode")
 	parseNoArgs(set, args, `
-Verifies a submit token. The input on stdin is either a raw hex
-token or a HTTP header. For a raw token, one of -k or --domain is
-required. For a HTTP header, --key and --domain are optional, but
-validation fails if they are inconsistent with what's looked up
-from the HTTP header. The -q (quiet) option suppresses output on
+Verify a submit token.  The input on stdin is either a raw hex token
+or an HTTP header.  For a raw token, one of --key or --domain is
+required.  For an HTTP header, --key and --domain are optional, but
+validation fails if they are inconsistent with what's looked up from
+the HTTP header.  The -q (quiet) option suppresses output on
 validation errors, with result only reflected in the exit code.
 `)
 }
